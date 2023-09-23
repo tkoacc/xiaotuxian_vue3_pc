e4bbccdc56e50427916fdc7d6a3a3e0c69e5bf67
TOP-Z\riley <riley211375@gmail.com>
2023-09-23 16:43:27 +0800

    layout

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,8 +1,12 @@
-<script setup></script>
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+</script>
 
 <template>
-  <div>layout</div>
-  <router-view></router-view>
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
 </template>
-
-<style lang="scss" scoped></style>

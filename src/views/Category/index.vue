9174a2ca20a9c78bb93b4a3a1df5e70f26c0c829
TOP-Z\riley <riley211375@gmail.com>
2023-10-07 22:01:55 +0800

    router

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -4,16 +4,21 @@ import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 import { getBannerAPI } from '@/apis/home'
 import GoodsItem from '../Home/components/GoodsItem.vue'
+import { onBeforeRouteUpdate } from 'vue-router'
 
 // 获取数据
 const categoryData = ref({})
 const route = useRoute()
-const getCategory = async () => {
+const getCategory = async (id = route.params.id) => {
   // 如何在setup中获取路由参数 useRoute() -> route 等价于this.$route
-  const res = await getCategoryAPI(route.params.id)
+  const res = await getCategoryAPI(id)
   categoryData.value = res.result
 }
 onMounted(() => getCategory())
+// 目标：路由参数变化的时候 可以把分类数据接口重新发送
+onBeforeRouteUpdate((to) => {
+  getCategory(to.params.id)
+})
 
 // 轮播图
 const bannerList = ref([])

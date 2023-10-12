cbe9399330ef627934fcc3c5742a391fecc33334
TOP-Z\riley <riley211375@gmail.com>
2023-10-13 00:49:27 +0800

    image

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -1,5 +1,6 @@
 <script setup>
 import DetailHot from './components/DetailHot.vue'
+import ImageView from '@/components/ImageView/index.vue'
 import { getDetail } from '@/apis/detail'
 import { useRoute } from 'vue-router'
 import { ref, onMounted } from 'vue'
@@ -39,7 +40,7 @@ onMounted(() => {
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-
+              <ImageView />
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>

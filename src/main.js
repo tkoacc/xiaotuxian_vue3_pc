44794e5e045c3aec26fd019ea8e2be56fb51c40f
TOP-Z\riley <riley211375@gmail.com>
2023-10-15 15:55:01 +0800

    pinia

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import '@/styles/common.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
@@ -12,8 +13,10 @@ import { lazyPlugin } from '@/directives'
 import { componentPlugin } from '@/components'
 
 const app = createApp(App)
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 app.use(lazyPlugin)
 app.use(componentPlugin)

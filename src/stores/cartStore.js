eb81dfec6e5e324f12bc486e24c16de49f376c50
TOP-Z\riley <riley211375@gmail.com>
2023-10-16 16:43:00 +0800

    APIcart

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,26 +1,39 @@
 // 封装购物车模块
 import { defineStore } from 'pinia'
 import { ref, computed } from 'vue'
+import { useUserStore } from './user'
+import { insertCartAPI, findNewCartListAPI } from '@/apis/cart'
 
 export const useCartStore = defineStore(
   'cart',
   () => {
+    const userStore = useUserStore()
+    const isLogin = computed(() => userStore.userInfo.token)
     // 1. 定义state - cartList
     const cartList = ref([])
     // 2. 定义action - addCart
-    const addCart = (goods) => {
-      // console.log('添加', goods)
-      // 添加购物车操作
-      // 已添加过 - count + 1
-      // 没有添加过 - 直接push
-      // 思路：通过匹配传递过来的商品对象中的skuId能不能在cartList中找到，找到了就是添加过
-      const item = cartList.value.find((item) => goods.skuId === item.skuId)
-      if (item) {
-        // 找到了
-        item.count++
+    const addCart = async (goods) => {
+      const { skuId, count } = goods
+      if (isLogin.value) {
+        // 登录之后的加入购物车逻辑
+        await insertCartAPI({ skuId, count })
+        // 获取最新的购物车列表
+        const res = await findNewCartListAPI()
+        // 覆盖本地的购物车列表
+        cartList.value = res.result
       } else {
-        // 没找到
-        cartList.value.push(goods)
+        // 添加购物车操作
+        // 已添加过 - count + 1
+        // 没有添加过 - 直接push
+        // 思路：通过匹配传递过来的商品对象中的skuId能不能在cartList中找到，找到了就是添加过
+        const item = cartList.value.find((item) => goods.skuId === item.skuId)
+        if (item) {
+          // 找到了
+          item.count++
+        } else {
+          // 没找到
+          cartList.value.push(goods)
+        }
       }
     }
     // 删除购物车
@@ -49,6 +62,19 @@ export const useCartStore = defineStore(
       cartList.value.reduce((pre, item) => pre + item.count * item.price, 0)
     )
 
+    // 3. 已选择数量
+    const selectedCount = computed(() =>
+      cartList.value
+        .filter((item) => item.selected)
+        .reduce((pre, item) => pre + item.count, 0)
+    )
+    // 4. 已选择商品价钱合计
+    const selectedPrice = computed(() =>
+      cartList.value
+        .filter((item) => item.selected)
+        .reduce((pre, item) => pre + item.count * item.price, 0)
+    )
+
     // 全选功能action
     const allCheck = (selected) => {
       // 把cartList中的每一项的selected都设置为当前的全选框状态
@@ -66,7 +92,9 @@ export const useCartStore = defineStore(
       allPrice,
       singleCheck,
       allCheck,
-      isAll
+      isAll,
+      selectedCount,
+      selectedPrice
     }
   },
   {
